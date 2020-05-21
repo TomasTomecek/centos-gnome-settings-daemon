1a51ad84f9de1167f43f055588595cf1149ad0f5
Tomas Tomecek <ttomecek@redhat.com>
2020-05-21 15:59:53 +0200

    Unpack archive

---
+++ plugins/keyboard/main.c
@@ -0,0 +1,8 @@
+#define NEW gsd_keyboard_manager_new
+#define START gsd_keyboard_manager_start
+#define STOP gsd_keyboard_manager_stop
+#define MANAGER GsdKeyboardManager
+#define GDK_BACKEND "x11"
+#include "gsd-keyboard-manager.h"
+
+#include "daemon-skeleton-gtk.h"
---
+++ plugins/media-keys/main.c
@@ -0,0 +1,8 @@
+#define NEW gsd_media_keys_manager_new
+#define START gsd_media_keys_manager_start
+#define STOP gsd_media_keys_manager_stop
+#define MANAGER GsdMediaKeysManager
+#define GDK_BACKEND "x11"
+#include "gsd-media-keys-manager.h"
+
+#include "daemon-skeleton-gtk.h"
